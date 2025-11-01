cc7fc4dfb91f27e436fcc6d931cb2d94b927a088
Stephen Deva Kumar Gona <gsdk630@gmail.com>
2025-11-01 13:17:15 +0530

    Updated code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,107 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnStart = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtHost = new System.Windows.Forms.TextBox();
-            this.txtPort = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnStop = new System.Windows.Forms.Button();
-            this.txtStatus = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(485, 48);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Host";
-            // 
-            // txtHost
-            // 
-            this.txtHost.Location = new System.Drawing.Point(169, 48);
-            this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(100, 22);
-            this.txtHost.TabIndex = 2;
-            this.txtHost.Text = "127.0.0.1";
-            // 
-            // txtPort
-            // 
-            this.txtPort.Location = new System.Drawing.Point(388, 48);
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(48, 22);
-            this.txtPort.TabIndex = 4;
-            this.txtPort.Text = "8910";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(309, 51);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Port";
-            // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(589, 48);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
-            this.btnStop.TabIndex = 5;
-            this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
-            // 
-            // txtStatus
-            // 
-            this.txtStatus.Location = new System.Drawing.Point(169, 91);
-            this.txtStatus.Multiline = true;
-            this.txtStatus.Name = "txtStatus";
-            this.txtStatus.Size = new System.Drawing.Size(495, 204);
-            this.txtStatus.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtStatus);
-            this.Controls.Add(this.btnStop);
-            this.Controls.Add(this.txtPort);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtHost);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Server";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtHost;
-        private System.Windows.Forms.TextBox txtPort;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.TextBox txtStatus;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,54 +1,246 @@
-﻿using System;
+﻿using ChatCommonn;
+using SimpleTCP;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SimpleTCP;
 
 namespace TCPChatApplication
 {
     public partial class Form1 : Form
     {
+        SimpleTcpServer server;
+        Dictionary<string, UserSession> activeUsers = new Dictionary<string, UserSession>();
+
+        private TextBox txtLogs;
+        private Button btnStart;
+        private Button btnStop;
+        private Label lblStatus;
+        private ListBox lstUsers;
+
         public Form1()
         {
             InitializeComponent();
-        }
+            Form1_Load(null, null);
+            this.Text = "TCP Chat Server";
+            this.ClientSize = new Size(650, 400);
 
-        SimpleTcpServer server;
+            Label lbl = new Label()
+            {
+                Text = "Server Logs",
+                Location = new Point(10, 10),
+                AutoSize = true
+            };
+            this.Controls.Add(lbl);
+
+            txtLogs = new TextBox()
+            {
+                Multiline = true,
+                ReadOnly = true,
+                ScrollBars = ScrollBars.Vertical,
+                Location = new Point(10, 30),
+                Size = new Size(450, 300)
+            };
+            this.Controls.Add(txtLogs);
+
+            lstUsers = new ListBox()
+            {
+                Location = new Point(480, 30),
+                Size = new Size(150, 300)
+            };
+            this.Controls.Add(lstUsers);
+
+            btnStart = new Button()
+            {
+                Text = "Start Server",
+                Location = new Point(10, 340),
+                Size = new Size(120, 30)
+            };
+            btnStart.Click += btnStart_Click;
+            this.Controls.Add(btnStart);
+
+            btnStop = new Button()
+            {
+                Text = "Stop Server",
+                Location = new Point(140, 340),
+                Size = new Size(120, 30)
+            };
+            btnStop.Click += btnStop_Click;
+            this.Controls.Add(btnStop);
+
+            lblStatus = new Label()
+            {
+                Text = "Server not started",
+                Location = new Point(280, 345),
+                AutoSize = true
+            };
+            this.Controls.Add(lblStatus);
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             server = new SimpleTcpServer();
-            server.Delimiter = 0x13; // enter
-            server.StringEncoder = Encoding.UTF8;
+            server.Delimiter = 0x13;
+            server.StringEncoder = System.Text.Encoding.UTF8;
             server.DataReceived += Server_DataReceived;
-        }
-
-        private void Server_DataReceived(object sender, SimpleTCP.Message e)
-        {
-           txtStatus.Invoke((MethodInvoker)delegate ()
-           {
-               txtStatus.Text+=e.MessageString;
-               e.ReplyLine(string.Format("You said: {0}", e.MessageString));
-           });
-
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            txtStatus.Text += "Server Starting....";
-            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text);
-            server.Start(ip,Convert.ToInt32(txtPort.Text));
+            try
+            {
+                var ip = IPAddress.Parse("127.0.0.1");
+                server.Start(ip, 9000);
+                lblStatus.Text = "✅ Server started on 127.0.0.1:9000";
+                Log("Server started...");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting server: " + ex.Message);
+            }
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            if(server.IsStarted)
+            if (server.IsStarted)
                 server.Stop();
         }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            try
+            {
+                var packet = JsonSerializer.Deserialize<Packet>(e.MessageString);
+                if (packet == null) return;
+
+                switch (packet.Type)
+                {
+                    case "LOGIN_REQ":
+                        HandleLogin(e, packet);
+                        break;
+                    case "DM":
+                        HandleDirectMessage(packet);
+                        break;
+                    case "MULTI":
+                        HandleMultiMessage(packet);
+                        break;
+                    case "BROADCAST":
+                        HandleBroadcast(packet);
+                        break;
+                }
+            }
+            catch
+            {
+                // Invalid JSON - close session
+                e.ReplyLine("Malformed JSON, session closed");
+            }
+        }
+
+        private void HandleLogin(SimpleTCP.Message e, Packet packet)
+        {
+            var validUsers = new Dictionary<string, string>
+            {
+                { "user1", "pass1" },
+                { "user2", "pass2" },
+                { "user3", "pass3" }
+            };
+
+            Packet response = new Packet() { Type = "LOGIN_RESP" };
+
+            if (validUsers.TryGetValue(packet.Username, out string pass) && pass == packet.Password)
+            {
+                response.Ok = true;
+                lock (activeUsers)
+                {
+                    activeUsers[packet.Username] = new UserSession { Username = packet.Username, Client = e.TcpClient };
+                }
+                Log($"{packet.Username} logged in");
+            }
+            else
+            {
+                response.Ok = false;
+                response.Reason = "Invalid credentials";
+            }
+
+            string json = JsonSerializer.Serialize(response);
+            e.ReplyLine(json);
+        }
+        private void SendMessageToClient(TcpClient client, string message)
+        {
+            if (client == null || !client.Connected) return;
+
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(message + "\n");
+                stream.Write(data, 0, data.Length);
+                stream.Flush();
+            }
+            catch (Exception ex)
+            {
+                Log($"Error sending to client: {ex.Message}");
+            }
+        }
+
+
+        private void HandleDirectMessage(Packet packet)
+        {
+            if (activeUsers.TryGetValue(packet.To, out UserSession receiver))
+            {
+                string json = JsonSerializer.Serialize(packet);
+                //receiver.Client.WriteLineAndGetReply(json, TimeSpan.FromSeconds(3));
+                SendMessageToClient(receiver.Client, json);
+
+                Log($"{packet.Username} → {packet.To}: {packet.Message}");
+            }
+        }
+
+        private void HandleMultiMessage(Packet packet)
+        {
+            foreach (var toUser in packet.ToList)
+            {
+                if (activeUsers.TryGetValue(toUser, out UserSession receiver))
+                {
+                    string json = JsonSerializer.Serialize(packet);
+                   // receiver.Client.WriteLineAndGetReply(json, TimeSpan.FromSeconds(3));
+                }
+            }
+            Log($"{packet.Username} → MULTI({string.Join(",", packet.ToList)}): {packet.Message}");
+        }
+
+
+        private void HandleBroadcast(Packet packet)
+        {
+            foreach (var kvp in activeUsers)
+            {
+                if (kvp.Key == packet.Username) continue; // don't send to sender
+
+                string json = JsonSerializer.Serialize(packet);
+
+                // Send using NetworkStream instead of WriteLineAndGetReply
+                SendMessageToClient(kvp.Value.Client, json);
+            }
+
+            Log($"{packet.Username} → BROADCAST: {packet.Message}");
+        }
+        
+
+
+
+        private void Log(string text)
+        {
+            txtLogs.Invoke((MethodInvoker)delegate
+            {
+                txtLogs.AppendText($"{DateTime.Now:T}: {text}\r\n");
+            });
+        } 
     }
 }
